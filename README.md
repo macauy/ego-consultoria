3199745257d1cee0c86b542ef01561ba787cb7fc
macauy <maca.rabosto@gmail.com>
2025-11-21 20:10:16 -0300

    seccion problemas

---
+++ README.md
@@ -0,0 +1,22 @@
+# EGO Consultoría
+
+Sitio web institucional desarrollado para **EGO Consultoría**, empresa dedicada a la gestión gastronómica y consultoría en restaurantes.
+
+## 🌐 Descripción
+
+Landing page moderna y responsiva con diseño exclusivo basado en Figma.  
+El sitio presenta los servicios de consultoría, experiencia profesional y opciones de contacto.
+
+## 🧩 Tecnologías utilizadas
+
+- **HTML5**
+- **CSS3** (Flexbox y Grid)
+- **JavaScript Vanilla**
+
+## 👩‍💻 Desarrollo
+
+Proyecto desarrollado por **Macarena Rabosto** (2025).  
+Diseño original realizado en Figma para EGO Consultoría.
+
+© 2025 EGO Consultoría.  
+Todos los derechos reservados. Reproducción o redistribución prohibida sin autorización.
---
+++ js/main.js
@@ -0,0 +1,39 @@
+const menuBtn = document.getElementById("menu-btn");
+const overlay = document.getElementById("overlay");
+const panel = document.getElementById("nav-panel");
+const icon = menuBtn.querySelector("img");
+const firstLink = panel.querySelector("a");
+
+const openMenu = () => {
+  menuBtn.setAttribute("aria-label", "Close Menu");
+  menuBtn.setAttribute("aria-expanded", "true");
+  icon.src = "./assets/icons/icon-close.svg";
+  panel.hidden = false;
+  overlay.hidden = false;
+  firstLink.focus();
+  overlay.classList.toggle("show");
+};
+
+const closeMenu = () => {
+  menuBtn.setAttribute("aria-label", "Open Menu");
+  menuBtn.setAttribute("aria-expanded", "false");
+  icon.src = "./assets/icons/icon-menu.svg";
+  panel.hidden = true;
+  overlay.hidden = true;
+  menuBtn.focus();
+  overlay.classList.toggle("show");
+};
+
+menuBtn.addEventListener("click", () => {
+  const isOpen = menuBtn.getAttribute("aria-expanded") == "true";
+  console.log("expended", menuBtn.getAttribute("aria-expanded"));
+  isOpen ? closeMenu() : openMenu();
+});
+
+overlay?.addEventListener("click", closeMenu);
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && menuBtn.getAttribute("aria-expanded") === "true") {
+    closeMenu();
+  }
+});
